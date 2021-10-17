c8f4e8bb63c84bdb52df98d8b922641d3302d4fe
“leonshimizu” <lmshimizu@gmail.com>
2021-10-17 12:39:35 -0700

    songs create

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,4 +19,14 @@ class SongsController < ApplicationController
     song = Song.find_by(id: params[:id])
     render json: song.as_json
   end
+
+  def update
+    song = Song.find_by(id: params[:id])
+    song.title = params[:title] || song.title
+    song.album = params[:album] || song.album
+    song.artist = params[:artist] || song.artist
+    song.year = params[:year] || song.year
+    song.save
+    render json: song.as_json
+  end
 end
