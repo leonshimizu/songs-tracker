8e2971a3bf4e062e586369d5d846d18b3acc0fe1
“leonshimizu” <lmshimizu@gmail.com>
2021-10-17 12:33:46 -0700

    songs sho

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,4 +14,9 @@ class SongsController < ApplicationController
     song.save
     render json: song.as_json
   end
+
+  def show
+    song = Song.find_by(id: params[:id])
+    render json: song.as_json
+  end
 end
